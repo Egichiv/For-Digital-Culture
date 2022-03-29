7c114e5b8cb92c1ca1a635519704fd9225046a46
Egichiv <egichiv@mail.ru>
2022-03-29 20:53:50 +0300

    Changed 'lazy' into 'productive'.

--- pseudo-proglab2.c
+++ pseudo-proglab2.c
@@ -3,6 +3,6 @@
 int main() {
     printf("This program is just an empty blanket for Digital Culture Labs\n");
     printf("Just so you could tell a difference: AAAAAA");
-    printf("It is cool to be lazy");
+    printf("It is cool to be productive");
     return 0;
 }
