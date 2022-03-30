e32e0880db0301ccd4af5a68e2221e4018a6ef71
Egichiv <egichiv@mail.ru>
2022-03-30 12:17:39 +0300

    Added printf feature in pseudo-proglab2.c

--- pseudo-proglab2.c
+++ pseudo-proglab2.c
@@ -3,5 +3,6 @@
 int main() {
     printf("This program is just an empty blanket for Digital Culture Labs\n");
     printf("Just so you could tell a difference: AAAAAA");
+    printf("It is cool to be lazy");
     return 0;
 }
